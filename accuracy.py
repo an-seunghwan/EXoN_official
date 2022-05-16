9a10bff411c349b53efe8da04c6eb0d7af1adce4
an-seunghwan <dpeltms79@gmail.com>
2022-05-16 13:35:21 +0900

    add accuracy code and result

---
+++ accuracy.py
@@ -0,0 +1,31 @@
+#%%
+import os
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+#%%
+import tensorflow as tf
+from tensorboard.backend.event_processing.event_accumulator import EventAccumulator
+import tensorboard as tb
+import numpy as np
+#%%
+"""classification error"""
+with open("accuracy.txt", "w") as file:
+    
+    for beta in [0.01, 0.05]:
+        dir = '/Users/anseunghwan/Documents/GitHub/EXoN_official/logs/cifar10_4000/repeated(beta_{})'.format(beta)
+        file_list = [x for x in os.listdir(dir) if x not in ['.DS_Store', 'datasets', 'etc']]
+        
+        error = []
+        for i in range(len(file_list)):
+            path = dir + '/{}/test'.format(file_list[i])
+            event_acc = EventAccumulator(path)
+            event_acc.Reload()
+            tag = 'accuracy'
+            event_list = event_acc.Tensors(tag)
+            value = tf.io.decode_raw(event_list[-1].tensor_proto.tensor_content, 
+                                    event_list[-1].tensor_proto.dtype)
+            error.append(100. * (1. - value.numpy()[0]))
+
+        file.write("{} | mean: {:.3f}, std: {:.3f}\n\n".format(beta, np.mean(error), np.std(error)))
+#%%
+"""inception score"""
+#%%
